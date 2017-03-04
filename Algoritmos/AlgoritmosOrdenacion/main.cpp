3f176302455ecea246d0e7d488baffcfb0324a4f
suavepimpi <alexis112297@gmail.com>
2017-03-04 13:02:34 -0600

    Update main.cpp

--- Algoritmos/AlgoritmosOrdenacion/main.cpp
+++ Algoritmos/AlgoritmosOrdenacion/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void llenarArreglo(int n);
 void ordenarArreglo (int arreglo[], int n);
+void ordenarBurbuja ();
 
 int main()
 {
@@ -40,5 +41,31 @@ void ordenarArreglo (int arreglo[], int n)
     primero = 0;
     ultimo = n-1;
     central = primero + ultimo / 2;
+}
+
+void ordenarBurbuja ()
+{
+    int num[5]={5,4,7,6,8};
+    int i,j,k,aux;
+
+    for(i=0;i<k;++i)
+    {
+        for(j=0;j<4;++j)
+        {
+            if (num[j]>num[j+1])
+            {
+                aux = num[j];
+                num[j] = num[j+1];
+                num[j+1]=aux;
+            }
+        }
+    }
+
+    for(k=0;k<5;++k){
+
+    printf("%i\n",num[k]);
+    }
+   return 0;
+    
 }
 
